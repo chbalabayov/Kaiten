19c655b8f4e063f28a3bf343e40aa5936c148825
shadowlabx <61518086+shadowlabx@users.noreply.github.com>
2020-05-11 00:14:54 -0400

    Update sign.py

--- lib/sign.py
+++ lib/sign.py
@@ -5,4 +5,4 @@ import os, sys
 def start():
     print('[!] set password as "toor"')
     os.system("openssl req -x509 -newkey rsa:4096 -keyout output/key.pem -out output/ignorecert.pem -days 365")
-    os.system('osslsigncode sign -certs "output/cert.pem" -key "output/key.pem" -pass "toor" -n "Microsoft" -i "https://www.Microsoft.com" -t "http://timestamp.comodoca.com/authenticode" -in "output/evil.exe" -out "output/signed.exe"')
+    os.system('osslsigncode sign -certs "output/cert.pem" -pass "toor" -key "output/key.pem" -n "Microsoft" -i "https://www.Microsoft.com" -t "http://timestamp.comodoca.com/authenticode" -in "output/evil.exe" -out "output/signed.exe"')
